52d5a72f1eff04133e14aea87b1ccde55cd5b085
Lala Yamazaki <lalayamazaki@Lalas-MBP.attlocal.net>
2022-12-08 16:16:32 -0600

    worked on task 4

--- js/TaskManager.js
+++ js/TaskManager.js
@@ -4,7 +4,7 @@ class TaskManager {
     this.currentId = currentId;
   }
 
-  addTask(name, description, assignedTo, dueDate) {
+  addTask({ name, description, assignedTo, dueDate }) {
     const task = {
       id: this.currentId++,
       name: name,
@@ -13,27 +13,29 @@ class TaskManager {
       dueDate: dueDate,
       status: "TODO",
     };
-
-    const task1 = {
-      id: 1,
-      name: "Take out the trash",
-      description: "Take out the trash to the front of the house",
-      assignedTo: "Nick",
-      dueDate: "2022-09-15",
-      status: "TODO",
-    };
-
-    const task2 = {
-      id: 2,
-      name: "Cook Dinner",
-      description:
-        "Prepare a healthy serving of pancakes for the family tonight",
-      assignedTo: "Nick",
-      dueDate: "2022-09-30",
-      status: "TODO",
-    };
-
-    // Push the task to the tasks property
     this.tasks.push(task);
   }
 }
+const newManager = new TaskManager();
+
+const task1 = {
+  id: 1,
+  name: "Take out the trash",
+  description: "Take out the trash to the front of the house",
+  assignedTo: "Nick",
+  dueDate: "2022-09-15",
+  status: "TODO",
+};
+
+const task2 = {
+  id: 2,
+  name: "Cook Dinner",
+  description: "Prepare a healthy serving of pancakes for the family tonight",
+  assignedTo: "Nick",
+  dueDate: "2022-09-30",
+  status: "TODO",
+};
+
+newManager.addTask(task1);
+newManager.addTask(task2);
+console.log(newManager.tasks);
